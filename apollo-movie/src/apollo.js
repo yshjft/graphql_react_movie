bfbf815efff013707a02f4d8fe45b429ab14e311
hong yuseok <jericoh@naver.com>
2020-11-10 22:28:42 +0900

    apollo-movie 1.1

--- apollo-movie/src/apollo.js
+++ apollo-movie/src/apollo.js
@@ -2,7 +2,7 @@ import ApolloClient from "apollo-boost";
 
 const client = new ApolloClient({
     // GraphQL API의 접속 정보를 설정
-    uri: "https://movieql.now.sh" // qraphql yoga 강의로 변경할 것
+    uri: "http://localhost:4000/" // qraphql yoga 강의로 변경할 것
 })
 
 export default client;
--- apollo-movie/src/routes/Home.js
+++ apollo-movie/src/routes/Home.js
@@ -1,2 +1,19 @@
-import React from "react"
-export default ()=><h1>Home</h1>;
+import React from "react";
+import {gql} from "apollo-boost";
+import {useQuery} from "@apollo/react-hooks";
+
+const GET_MOVIES = gql`
+    {
+        movies {
+            id
+            medium_cover_image
+        }
+    }
+`;
+
+export default ()=>{
+    const {loading, error, data} = useQuery(GET_MOVIES);
+    console.log(loading, data)
+
+    return (<h1>Home</h1>)
+};
