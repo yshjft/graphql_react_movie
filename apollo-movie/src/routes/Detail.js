c4446c8d5d4072c733cf52e5797cda96b84b7761
hong yuseok <jericoh@naver.com>
2020-11-11 22:18:46 +0900

    apollo movie 12

--- apollo-movie/src/routes/Detail.js
+++ apollo-movie/src/routes/Detail.js
@@ -1,2 +1,29 @@
 import React from "react"
-export default ()=><h1>Detail</h1>;
+import {useParams} from "react-router-dom";
+import {gql} from "apollo-boost";
+import {useQuery} from "@apollo/react-hooks";
+
+
+const GET_MOVIE = gql`
+    query getMovie($id: Int!){
+        movie(id: $id) {
+            id
+            title
+            medium_cover_image
+            description_intro
+        }
+    }
+`
+
+export default () => {
+    const {id} = useParams();
+    const {loading, data} = useQuery(GET_MOVIE, {
+        variables: {id: parseInt(id)}
+    });
+    if(loading){
+        return "loading"
+    }
+    if(data && data.movie){
+        return data.movie.title
+    }
+}
--- apollo-movie/src/routes/Home.js
+++ apollo-movie/src/routes/Home.js
@@ -1,6 +1,8 @@
 import React from "react";
 import {gql} from "apollo-boost";
 import {useQuery} from "@apollo/react-hooks";
+import styled from "styled-components";
+import Movie from "../components/Movie";
 
 const GET_MOVIES = gql`
     {
@@ -11,9 +13,60 @@ const GET_MOVIES = gql`
     }
 `;
 
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+`;
+
+const Header = styled.header`
+    background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+    height: 45vh;
+    color: white;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+`;
+
+const Title = styled.h1`
+    font-size: 60px;
+    font-weight: 600;
+    margin-bottom: 20px;
+`
+
+const Subtitle = styled.h3`
+  font-size: 35px;
+`;
+
+const Loading = styled.div`
+  font-size: 18px;
+  opacity: 0.5;
+  font-weight: 500;
+  margin-top: 10px;
+`;
+
+const Movies = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 25px;
+  width: 60%;
+  position: relative;
+  top: -50px;
+`;
+
 export default ()=>{
     const {loading, error, data} = useQuery(GET_MOVIES);
-    console.log(loading, data)
-
-    return (<h1>Home</h1>)
+    return (
+        <Container>
+            <Header>
+                <Title>Apollo 2020</Title>
+                <Subtitle>I love GraphQL</Subtitle>
+            </Header>
+            {loading && <Loading>...Loading</Loading>}
+            {!loading && data.movies && data.movies.map ((movie,index) => <Movie key={index} id={movie.id}/>)}
+        </Container>
+    )
 };
